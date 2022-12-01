a03678469d844de0ca60b0a54ec66d01a26d048d
Miguel Escudero <miguelescuderopablos@gmail.com>
2022-12-01 10:51:57 +0100

    Model structure

---
+++ wavewatcher/ml_logic/model.py
@@ -0,0 +1,98 @@
+from colorama import Fore, Style
+
+import time
+print(Fore.BLUE + "\nLoading tensorflow..." + Style.RESET_ALL)
+start = time.perf_counter()
+
+from tensorflow.keras import Model, Sequential, layers, regularizers, optimizers
+from tensorflow.keras.callbacks import EarlyStopping
+
+end = time.perf_counter()
+print(f"\n✅ tensorflow loaded ({round(end - start, 2)} secs)")
+
+from typing import Tuple
+
+import numpy as np
+
+def initialize_model(X: np.ndarray) -> Model:
+    """
+    Initialize the Neural Network with random weights
+    """
+    print(Fore.BLUE + "\nInitialize model..." + Style.RESET_ALL)
+
+    # Your model here
+
+    print("\n✅ model initialized")
+
+    return model
+
+def compile_model(model, learning_rate=1e-4) -> Model:
+    """
+    Compile the Neural Network
+    """
+    model.compile(loss = "categorical_crossentropy",
+                            optimizer = Adam(learning_rate=learning_rate),
+                            metrics = ["accuracy"])
+
+    print("\n✅ model compiled")
+    return model
+
+def train_model(model: Model,
+                X: np.ndarray,
+                y: np.ndarray,
+                batch_size=64,
+                patience=2,
+                validation_split=0.3,
+                validation_data=None) -> Tuple[Model, dict]:
+    """
+    Fit model and return a the tuple (fitted_model, history)
+    """
+
+    print(Fore.BLUE + "\nTrain model..." + Style.RESET_ALL)
+
+    es = EarlyStopping(monitor="val_loss",
+                       patience=patience,
+                       restore_best_weights=True,
+                       verbose=0)
+
+    history = model.fit(X,
+                        y,
+                        validation_split=validation_split,
+                        validation_data=validation_data,
+                        epochs=100,
+                        batch_size=batch_size,
+                        callbacks=[es],
+                        verbose=0)
+
+    print(f"\n✅ model trained ({len(X)} rows)")
+
+    return model, history
+
+def evaluate_model(model: Model,
+                   X: np.ndarray,
+                   y: np.ndarray,
+                   batch_size=64) -> Tuple[Model, dict]:
+    """
+    Evaluate trained model performance on dataset
+    """
+
+    print(Fore.BLUE + f"\nEvaluate model on {len(X)} rows..." + Style.RESET_ALL)
+
+    if model is None:
+        print(f"\n❌ no model to evaluate")
+        return None
+
+    metrics = model.evaluate(
+        x=X,
+        y=y,
+        batch_size=batch_size,
+        verbose=1,
+        # callbacks=None,
+        return_dict=True)
+
+    loss = metrics["loss"]
+    accuracy = metrics["accuracy"]
+
+    print(f"\n✅ model evaluated: loss {round(loss, 2)} accuracy {round(accuracy, 2)}")
+
+    return metrics
