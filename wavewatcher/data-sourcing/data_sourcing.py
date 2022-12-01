1ea3cf9015d640834774319024559f4ae80c13e2
IamjustNick <nicolasmgeijo@gmail.com>
2022-12-01 15:08:41 +0100

    Updated

---
+++ wavewatcher/data-sourcing/data_sourcing.py
@@ -0,0 +1,39 @@
+from google.cloud import storage
+import os
+from google.cloud import bigquery
+import pandas as pd
+from colorama import Fore, Style
+import zipfile
+
+
+def download_blob(bucket_name, parent_blob: str):
+    """Downloads a blob from the bucket."""
+    #create client
+    storage_client = storage.Client()
+
+    #fetch blob
+    blobs = storage_client.list_blobs(bucket_name)
+
+    #make host directory if it doesn't exist
+    if not os.path.isdir(parent_blob):
+        os.mkdir(parent_blob)
+
+    for blob in blobs:
+        #where I want it stored!
+        blob.download_to_filename(blob.name)
+    return True
+
+
+
+
+def extract_to(parent_blob):
+    ##Unzips all files in our parent blob we just downloaded from GCS
+    for zipped_file in os.listdir(parent_blob):
+        print(f"Unzipping {zipped_file}...")
+        with zipfile.ZipFile(os.path.join(parent_blob, zipped_file),
+                             'r') as zip_ref:
+            #we do not define an extraction directory since the zipped files already contain
+            #the directories, since there was a large folder containing
+            #all subsequent folders with files belonging to each category
+            zip_ref.extractall()
+        print("Success!")
