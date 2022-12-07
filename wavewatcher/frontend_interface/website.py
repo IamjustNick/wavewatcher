dc3da601ab45586205734ce6e7630adf8f515408
IamjustNick <84037153+IamjustNick@users.noreply.github.com>
2022-12-07 10:54:51 +0100

    Update website.py

--- wavewatcher/frontend_interface/website.py
+++ wavewatcher/frontend_interface/website.py
@@ -10,21 +10,20 @@ from google.oauth2 import service_account
 from google.cloud import storage
 
 #----------Function for adding a background image------------------------------
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
+def add_bg_from_url():
     st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local('//wavewatcher/wavewatcher/frontend_interface/backgroundimage.png')
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://github.com/IamjustNick/wavewatcher/blob/master/wavewatcher/frontend_interface/backgroundimage.png?raw=true");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+add_bg_from_url()
 
 
 #----------Credentials for using Google Cloud storage -------------------------
